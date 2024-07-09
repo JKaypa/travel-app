705029691cb3df0e2f4c0b9f5a60dfd404724d3e
JKaypa <jose_kaypa@hotmail.com>
2024-07-09 17:01:01 -0500

    feat: + routes enum

---
+++ src/enums/routes/routes.enum.ts
@@ -0,0 +1,10 @@
+const Route = {
+  BOOKINGS: "/bookings",
+  ROOT: "/",
+  SIGNIN: "/sign-in",
+  SIGNUP: "/sign-up",
+  TRIP_ID: "/trip:tripId",
+  TRIP: "/trip/",
+} as const;
+
+export { Route };
