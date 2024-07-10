436ca542e3775b8f1e91a18749f0b1ae84277787
JKaypa <jose_kaypa@hotmail.com>
2024-07-10 13:40:56 -0500

    feat: + export button component

--- src/components/components.ts
+++ src/components/components.ts
@@ -2,4 +2,4 @@ export { Router } from "./router/router";
 export { Header } from "./header/header";
 export { Footer } from "./footer/footer";
 export { Input } from "./input/input";
-export { Button } from "./butoon/button";
+export { Button } from "./button/button";
