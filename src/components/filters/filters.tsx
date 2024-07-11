3f3d95e6aa088fe353124e4f22af0f42e72ddad6
JKaypa <jose_kaypa@hotmail.com>
2024-07-11 14:59:30 -0500

    feat: + create filters component

---
+++ src/components/filters/filters.tsx
@@ -0,0 +1,34 @@
+import { ChangeEvent } from "react";
+import { Input, Selection } from "../components";
+import { duration, level } from "../selection/helpers/options.helper";
+import { InputTest, InputType, Label, Name, PlaceHolder } from "~/enums/enums";
+import "./styles/filters.css";
+
+interface Props {
+  handleChange: (event: ChangeEvent<HTMLSelectElement | HTMLInputElement>) => void;
+}
+
+const Filters = ({ handleChange }: Props) => {
+  return (
+    <section className="trips-filter">
+      <h2 className="visually-hidden">Trips filter</h2>
+      <form className="trips-filter__form" autoComplete="off">
+        <Input
+          label={Label.SEARCH_BY_NAME}
+          name={Name.SEARCH}
+          testId={InputTest.SEARCH}
+          type={InputType.SEARCH}
+          placeHolder={PlaceHolder.SEARCH_TITLE}
+          filterCls="trips-filter__search"
+          labelCls="visually-hidden"
+          handleChange={handleChange}
+          required={false}
+        />
+        <Selection filteredBy="duration" options={duration} handleChange={handleChange} />
+        <Selection filteredBy="level" options={level} handleChange={handleChange} />
+      </form>
+    </section>
+  );
+};
+
+export { Filters };
