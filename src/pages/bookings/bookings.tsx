af548a350422402bbbd66a46ec09be8e65587a91
JKaypa <jose_kaypa@hotmail.com>
2024-07-12 16:29:04 -0500

    feat: + create bookings page

---
+++ src/pages/bookings/bookings.tsx
@@ -0,0 +1,34 @@
+import dataBookings from "~/data/bookings.json";
+import { BookingCard } from "~/components/components";
+import { useFormatDate } from "~/hooks/hooks";
+import "./styles/bookings.css";
+
+type Props = {
+  bookings: typeof dataBookings;
+  handleClose: (id: string) => void;
+};
+
+const Bookings = ({ bookings, handleClose }: Props) => {
+  const bookingsFormatted = useFormatDate(bookings);
+
+  return (
+    <main className="bookings-page">
+      <h1 className="visually-hidden">Travel App</h1>
+      <ul className="bookings__list">
+        {bookingsFormatted?.map((booking) => (
+          <BookingCard
+            id={booking.id}
+            title={booking.trip.title}
+            guests={booking.guests}
+            date={booking.date}
+            totalPrice={booking.totalPrice}
+            handleClose={handleClose}
+            key={booking.id}
+          />
+        ))}
+      </ul>
+    </main>
+  );
+};
+
+export { Bookings, type Props as NewBooking };
