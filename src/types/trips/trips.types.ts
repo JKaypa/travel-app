faf38c5c7b35418f495957fbe5714b52e5b765c0
JKaypa <jose_kaypa@hotmail.com>
2024-07-12 16:34:26 -0500

    feat: + add date and guests property types

--- src/types/trips/trips.types.ts
+++ src/types/trips/trips.types.ts
@@ -1,8 +1,17 @@
+import { TripCardProps } from "~/components/trip-card/trip-card";
+
 type TripSetters = {
+  tripDate: string;
+  tripGuests: number;
   tripPrice: number;
   tripTitle: string;
   tripId: string;
   tripDuration: number;
 };
 
-export { type TripSetters };
+type Trip = TripCardProps & {
+  description: string;
+  createdAt: string;
+};
+
+export { type Trip, type TripSetters };
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,4 @@
 export { type ValueOf } from "./value-of/value-of.types";
-export { type TripSetters } from "./trips/trips.types";
+export { type Trip, type TripSetters } from "./trips/trips.types";
 export { type TripProps } from "~/pages/trip-detail/trip-detail";
+export { type Booking } from "./bookings/bookings.type";
