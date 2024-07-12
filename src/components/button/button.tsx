c937397e071744c0777cd696fc65f88e6d38ae7c
JKaypa <jose_kaypa@hotmail.com>
2024-07-12 16:18:29 -0500

    feat: + add title

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,11 +1,12 @@
-import { BtnCls, BtnTest, Title } from "~/enums/enums";
+import { BtnCls, BtnTest, BtnChild } from "~/enums/enums";
 import { ValueOf } from "~/types/types";
 
 type Props = {
   testId: ValueOf<typeof BtnTest>;
   cls: ValueOf<typeof BtnCls>;
   type?: "submit";
-  children: JSX.Element | ValueOf<typeof Title>;
+  children?: JSX.Element | ValueOf<typeof BtnChild>;
+  title?: "Cancel booking";
   onClick?: () => void;
 };
 
@@ -15,9 +16,9 @@ const cancelBooking = (
   </>
 );
 
-const Button = ({ children = cancelBooking, cls, testId, type, onClick }: Props) => {
+const Button = ({ children = cancelBooking, cls, testId, title, type, onClick }: Props) => {
   return (
-    <button data-test-id={testId} className={cls} type={type} onClick={onClick}>
+    <button data-test-id={testId} className={cls} type={type} title={title} onClick={onClick}>
       {children}
     </button>
   );
