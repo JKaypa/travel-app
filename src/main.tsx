c2b8b43e0e5ba936593c5813feeb514a6ebda2fb
JKaypa <jose_kaypa@hotmail.com>
2024-07-16 20:26:55 -0500

    feat: + add provider store and toast container components

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
+import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ToastContainer position="top-right" />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
